f7382d250b5902417ae680f27cdb7888b71b2672
Tanusree-Das <120117735+Tanusree-Das@users.noreply.github.com>
2023-01-09 12:06:40 +0000

    chatgpt code added

--- hackerRank/decideCompanyLogo.py
+++ hackerRank/decideCompanyLogo.py
@@ -6,33 +6,47 @@ Print the three most common characters along with their occurrence count.
 Sort in descending order of occurrence count.
 If the occurrence count is the same, sort the characters in alphabetical order.'''
 
-import collections
+from collections import Counter
 
 def get_key_from_value(d, val):
     keys = [k for k, v in d.items() if v == val]
     keys.sort()
     return keys
 
-if __name__ == '__main__':
-    s = input()
-    final_dict=dict()
-    list_to_work_with=list()
-    x=collections.Counter([*s])
-    full_dict=dict(x)
+def solution_by_tannu(s):
+    final_dict = dict()
+    list_to_work_with = list()
+    x = Counter([*s])
+    full_dict = dict(x)
     top3 = x.most_common(3)
-    top3_dict=dict(top3)
-    print(top3_dict)
+    top3_dict = dict(top3)
     counter = 0
 
-    for i in top3_dict.values(): #sort by alphabet
-        list_to_work_with.append(get_key_from_value(full_dict,i))
+    for i in top3_dict.values():  # sort by alphabet
+        list_to_work_with.append(get_key_from_value(full_dict, i))
         for j in list_to_work_with[0]:
-            final_dict[j]=i
-        list_to_work_with=[]
-    for i,j in final_dict.items():
-        if counter<3:
-            print(i,j)
-            counter=counter+1
+            final_dict[j] = i
+        list_to_work_with = []
+    for i, j in final_dict.items():
+        if counter < 3:
+            print(i, j)
+            counter = counter + 1
+
+def solution_by_chatgpt(s):
+    char_count=Counter(s)
+    sorted_char_count=sorted(char_count.items(), key=lambda x:(-x[1],x[0])) #need to read this part
+    for i,j in dict(sorted_char_count[:3]).items():
+        print(i,j)
+
+
+
+if __name__ == '__main__':
+    s = input()
+    print("solution by tannu")
+    solution_by_tannu(s)
+    print("solution by chatgpt")
+    solution_by_chatgpt(s)
+
 
 
 
